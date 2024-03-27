4863c3020ffbfd96541c06c9e86b83c582ad8059
JMGarCas <jmgarciacasillas135@gmail.com>
2024-03-27 13:48:55 +0100

    feat: footer

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -18,7 +18,7 @@ function App() {
   }, []);
 
   return (
-    <>
+    <main className="min-h-screen">
       <h1 className="text-center text-2xl py-5 bg-[#333]">
         ☀️ ENERGY DASHBOARD ☀️
       </h1>
@@ -32,7 +32,7 @@ function App() {
       ) : (
         <p>Loading...</p>
       )}
-    </>
+    </main>
   );
 }
 
